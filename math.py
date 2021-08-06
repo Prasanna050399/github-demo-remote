2b8e052f4df8269b28cfc2eee77a638cb02fbcaf
Prasanna050399 <56738929+Prasanna050399@users.noreply.github.com>
2021-08-06 16:58:35 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#add implementation
+def add(x,y):
+    return x+y
+
+#subtract implementation
+def sub(x,y):
+    pass
+
+#multiplication implementation
+def mult(x,y):
+    return x*y
+
+#division implementation
+def div(x,y):
+    if y == 0:
+        return None
+    else:
+        return x/y
+
+#square implementation 
+def square(x):
+    pass
